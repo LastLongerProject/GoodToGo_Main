5f99b126c1f878580edaf04887e064a8c5a335e1
BenjaminChenTW <s910190benjamin@gmail.com>
2022-05-16 16:31:35 +0800

    Update package and GA

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,76 @@
-var gulp = require('gulp'),
-    browserSync = require('browser-sync'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    prefix = require('gulp-autoprefixer'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    addsrc = require('gulp-add-src'),
-    gulpNotify = require("gulp-notify");
-
-sass.compiler = require('node-sass');
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
+const addSrc = require("gulp-add-src");
+const notify = require("gulp-notify");
 
 function js() {
-    return gulp.src('resources/js/jquery-3.2.1.min.js')
-        .pipe(addsrc.append('resources/js/bootstrap.min.js'))
-        .pipe(addsrc.append('resources/js/jquery.easing.min.js'))
-        .pipe(addsrc.append('resources/js/parallax.min.js'))
-        .pipe(addsrc.append('resources/js/scrolling-nav.js'))
-        .pipe(addsrc.append('resources/js/jquery.scrollify.js'))
-        .pipe(addsrc.append('resources/js/jquery.waypoints.min.js'))
-        .pipe(addsrc.append('resources/js/app.js'))
-        .pipe(sourcemaps.init())
-        .pipe(concat('app.min.js'))
-        .pipe(uglify().on("error", gulpNotify.onError(function (error) {
-            return "Js 編譯發生錯誤： " + error;
-        })))
-        .pipe(sourcemaps.write('source-maps'))
-        .pipe(gulp.dest('build/assets/js/'))
-        .pipe(gulpNotify("js 檔案混淆成功"));
+  return gulp
+    .src("resources/js/jquery-3.2.1.min.js")
+    .pipe(addSrc.append("resources/js/bootstrap.min.js"))
+    .pipe(addSrc.append("resources/js/jquery.easing.min.js"))
+    .pipe(addSrc.append("resources/js/parallax.min.js"))
+    .pipe(addSrc.append("resources/js/scrolling-nav.js"))
+    .pipe(addSrc.append("resources/js/jquery.scrollify.js"))
+    .pipe(addSrc.append("resources/js/jquery.waypoints.min.js"))
+    .pipe(addSrc.append("resources/js/app.js"))
+    .pipe(sourcemaps.init())
+    .pipe(concat("app.min.js"))
+    .pipe(
+      uglify().on(
+        "error",
+        notify.onError(function (error) {
+          return "Js 編譯發生錯誤： " + error;
+        })
+      )
+    )
+    .pipe(sourcemaps.write("./map"))
+    .pipe(gulp.dest("build/assets/js/"))
+    .pipe(notify("js 檔案混淆成功"));
 }
 
 function css() {
-    return gulp.src('resources/sass/pages/*.sass')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            sourcemap: true,
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
-        .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('build/assets/css'))
-        .pipe(gulpNotify("css 編譯成功"));
+  return gulp
+    .src("resources/sass/pages/*.sass")
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        outputStyle: "compressed",
+      }).on("error", sass.logError)
+    )
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write("./map"))
+    .pipe(gulp.dest("build/assets/css"))
+    .pipe(notify("css 編譯成功"));
 }
 
 function browser_sync() {
-    browserSync.init({
-        server: {
-            baseDir: "./build/"
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: "./build/",
+    },
+  });
 
-    gulp.watch(['build/**/*.*']).on('change', function () {
-        browserSync.reload();
-    });
+  gulp.watch(["build/**/*.*"]).on("change", function () {
+    browserSync.reload();
+  });
 }
 
 function watch() {
-    gulp.watch("resources/sass/**/*.sass", css);
-    gulp.watch("resources/js/*.js", js);
+  gulp.watch("resources/sass/**/*.sass", css);
+  gulp.watch("resources/js/*.js", js);
 }
 
 const build_assets = gulp.parallel(js, css);
-const develop = gulp.series(gulp.parallel(js, css), gulp.parallel(browser_sync, watch));
+const develop = gulp.series(
+  gulp.parallel(js, css),
+  gulp.parallel(browser_sync, watch)
+);
 
-gulp.task('default', develop);
-gulp.task('build', build_assets);
+gulp.task("default", develop);
+gulp.task("build", build_assets);
